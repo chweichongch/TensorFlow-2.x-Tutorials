7e5ac919a2c99f02ee6ef666317ebfa173ccdfa1
chweichongch <chwei@bjtu.edu.cn>
2019-11-24 10:44:06 +0800

    Update faster_rcnn.py

--- 16-fasterRCNN/detection/models/detectors/faster_rcnn.py
+++ 16-fasterRCNN/detection/models/detectors/faster_rcnn.py
@@ -108,6 +108,32 @@ class FasterRCNN(tf.keras.Model, RPNTestMixin, BBoxTestMixin):
         :param inputs: [1, 1216, 1216, 3], [1, 11], [1, 14, 4], [1, 14]
         :param training:
         :return:
+        
+        输入数据
+        训练：图像imgs，img_metas, gt_boxes真实边框的位置最多14个框, 真实边框的种类最多14个框 
+        预测：图像imgs，img_metas
+        
+        用backbone函数得到C2，C3，C4，C5几个不同感受野的特征图
+        
+        将C2，C3，C4，C5输入到neck函数里得到FPN输出的P2, P3, P4, P5, P6几个不同的感受野的特征图
+        
+        rpn_feature_maps = [P2, P3, P4, P5, P6]为rpn的特征图集合
+        rcnn_feature_maps = [P2, P3, P4, P5]为rcnn的特征图集合
+        
+        将rpn的特征图集合输入到rpn_head函数里得到rpn网络输出的穷举边框的分类效用rpn_class_logits，穷举边框的分类概率rpn_probs，
+        穷举边框的位置rpn_deltas
+        
+        将穷举边框的分类概率rpn_probs，穷举边框的位置rpn_deltas输入到rpn_head.get_proposals函数里得到选取的建议边框名单proposals_list
+        
+        输入建议边框名单rois_list = proposals_list, rcnn的特征图集合rcnn_feature_maps, img_metas到roi_align函数里得到了
+        从原特征图上为每个建议边框抠出来的子特征图集合pooled_regions_list，每张子图的尺寸被resize为了[7,7]
+        
+        将抠出的特征子图集合pooled_regions_list输入到函数bbox_head中，
+        得到rcnn_class_logits_list, rcnn_probs_list, rcnn_deltas_list 
+        
+        将rcnn_class_logits_list, rcnn_probs_list, rcnn_deltas_list输入到函数bbox_head.get_bboxes得到最终的预测边框detections_list
+            
+        
         """
         if training: # training
             imgs, img_metas, gt_boxes, gt_class_ids = inputs
